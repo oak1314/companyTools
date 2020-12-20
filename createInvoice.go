d0291006e03114b2ebf0dec486708e542b5d93e0
oak1314 <oak1314@gmail.com>
2020-12-20 17:35:05 +0900

    Add files via upload
    各会社の支払いサイト対応

--- createInvoice.go
+++ createInvoice.go
@@ -13,10 +13,11 @@ const templateData string = "./作成用データ/請求書テンプレート.xl
 
 // Invoice 請求書
 type Invoice struct {
-	cellCompany string
-	cellMonth   string
-	cellNames   []string
-	cellMoney   []string
+	cellCompany  string
+	cellMonth    string
+	cellPayMonth string
+	cellNames    []string
+	cellMoney    []string
 }
 
 func main() {
@@ -35,6 +36,7 @@ func main() {
 	currentLine := 3
 	currentCompany := ""
 	currentMonth := ""
+	currentPayMonth := ""
 	for {
 		cellCompany, err := f.GetCellValue("Sheet1", fmt.Sprintf("B%d", currentLine))
 		if err != nil {
@@ -48,22 +50,33 @@ func main() {
 			return
 		}
 
+		cellPayMonth, err := f.GetCellValue("Sheet1", fmt.Sprintf("J%d", currentLine))
+		if err != nil {
+			println(err.Error())
+			return
+		}
+
 		if currentLine == 3 {
 			// 一回目
 			currentCompany = cellCompany
 			currentMonth = cellMonth
+			currentPayMonth = cellPayMonth
 		}
 
 		// 会社が変わる
 		if cellCompany != currentCompany && currentCompany != "" {
+			// 一個前の会社フィアルを生成
 			inv.cellCompany = currentCompany
 			inv.cellMonth = currentMonth
+			inv.cellPayMonth = currentPayMonth
 			inv.cellNames = names
 			inv.cellMoney = moneys
 			createNewExcel(*inv)
 
+			// 現在の会社を代入
 			currentCompany = cellCompany
 			currentMonth = cellMonth
+			currentPayMonth = cellPayMonth
 			names = names[:0]
 			moneys = moneys[:0]
 		}
@@ -111,14 +124,26 @@ func createNewExcel(inv Invoice) {
 		println(err.Error())
 		return
 	}
-
+	// 請求会社名
 	f.SetCellDefault("Sheet1", "B9", inv.cellCompany+"　御中")
+	// 印刷エリア外(月)
 	f.SetCellDefault("Sheet1", "B3", inv.cellMonth)
+	// お振込期限
+	if inv.cellPayMonth == "1" {
+		f.SetCellFormula("Sheet1", "B38", "EOMONTH(DATE(YEAR(NOW()),$B$3,1),1)")
+	}else if inv.cellPayMonth == "1.5" {
+		f.SetCellFormula("Sheet1", "B38", "EOMONTH(DATE(YEAR(NOW()),$B$3,1),1)+DAY(15)")
+	}else if inv.cellPayMonth == "2" {
+		f.SetCellFormula("Sheet1", "B38", "EOMONTH(DATE(YEAR(NOW()),$B$3,1),2)")
+	}
+	//f.CalcCellValue("Sheet1", "B38")
 
+	// 印刷エリア外(氏名)
 	for index, cellName := range inv.cellNames {
 		f.SetCellDefault("Sheet1", fmt.Sprintf("A%d", 3+index), cellName)
 	}
 	for index, money := range inv.cellMoney {
+		// 単価
 		f.SetCellDefault("Sheet1", fmt.Sprintf("E%d", 20+index), money)
 		if index == 0 {
 			continue
